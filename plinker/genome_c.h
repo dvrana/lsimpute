ec25e4434f925039ee124291000a210699be94a8
DT Vrana <dtv@andrew.cmu.edu>
2018-04-12 23:16:51 -0400

    Changed interface

--- plinker/genome_c.h
+++ plinker/genome_c.h
@@ -9,14 +9,31 @@ typedef struct genome {
 } genome_t;
 
 // possibly may change
-genome_t *g_fromfile(FILE *ped, FILE *map);
+genome_t* g_fromfile(FILE* ped, FILE* map);
 
-void *g_filterby(genome_t *g, genome_t *filt);
+// number of individuals
+int g_nsample(genome_t* g);
 
-snp_t *g_plookup(genome_t *g, int id);
-snp_t *g_slookup(genome_t *g, int id);
+// number of SNPs
+int g_nsnp(genome_t* g)
 
-// possibly might want to use a different type
-size_t s_absdst(snp_t *);
-size_t s_pairdst(snp_t *, snp_t *);
+// Removes all SNPs from genome g not present in filt
+void* g_filterby(genome_t* g, genome_t* filt);
+
+// Filters out genomes not present among n given people in array ids
+void* g_filterindiv(genome_t* g, int* ids, int n);
+
+// Filters out all 
+// Dylan's note to Cam, delete when done:
+// Humans have 22 autosomes (present in everyone, two copies each) plus sex
+// chromosomes and mitochondrial DNA.  That's complex.  For a first pass, remove
+// everything not an autosome (chromosomes 1-22).
+void* g_filterchrom(genome_t* g, int chromosome);
+
+// What do these do?
+snp_t *g_plookup(genome_t* g, int id);
+snp_t *g_slookup(genome_t* g, int id);
+
+// Recombinant distance (in centimorgans) between x and y
+double s_pairdst(snp_t* x, snp_t* y);
 
