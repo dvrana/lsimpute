cdc68e9951416f074371d47363349c09df265df2
DT Vrana <dtv@andrew.cmu.edu>
2018-04-27 16:52:00 -0400

    CUDA notes and type

--- src/lsimpute.cu
+++ src/lsimpute.cu
@@ -1,7 +1,19 @@
 
 #include <lsimpute.h>
 
-void lsimpute::compute() {
+/* Returns smoothed Li-Stephens probabilities as a two-dimensional,
+ * heap-allocated array A[s][n], where s is the number of SNPs and n the number
+ * of reference genomes, and A[i][j] is the natural log of the probability that
+ * the ancestor is reference genome j at SNP i.
+ *
+ * Probabilities are for genome id from sample, using ref as a reference panel.
+ * g  - Garble rate- probability that a test haplotype doesn't line up with
+ *   reference from which it comes.
+ * theta - Recombination rate constant, s.t. jump probability is
+ *   1-e^{-theta d}, where d is distance in centimorgans
+ */
+float* lsimpute::compute() {
   // do the thing!
+  return NULL; // TODO: this
 }
 
