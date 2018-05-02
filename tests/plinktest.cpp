e7ecd207db6b495fa779e33034be6c4dcfdc6f17
CT075 <camtech075@gmail.com>
2018-05-02 12:58:59 -0400

    make plinktest good

--- tests/plinktest.cpp
+++ tests/plinktest.cpp
@@ -11,10 +11,12 @@ const char* MAP_TEST = "data/plinkdata.map";
 void runPlinkBasicTest() {
     genome_t g = g_fromfile(std::string(PED_TEST), std::string(MAP_TEST));
 
+    fprintf(stderr, "Loaded genome successfully.\n");
     ASSERT(g_nsample(g) == 2, "genome_t should report the correct nsample");
     ASSERT(g_nsnp(g) == 3, "genome_t should report the correct nsnp");
 
-    auto s = g_plookup(g, (char *)"01_01");
+    auto s = g_plookup(g, (char *)"01_1");
+    fprintf(stderr, "Looked up sample successfully.\n");
 
     ASSERT(s_query(s[0], A), "first allele not correctly recorded");
     ASSERT(s_query(s[0], G), "second allele not correctly recorded");
@@ -22,7 +24,8 @@ void runPlinkBasicTest() {
     ASSERT(s_query(s[2], A), "last pair of alleles not recorded");
     ASSERT(s_query(s[2], C), "last pair of alleles not recorded");
 
-    auto s2 = g_plookup(g, (char*)"01_02");
+    auto s2 = g_plookup(g, (char*)"01_2");
+    fprintf(stderr, "Looked up sample successfully.\n");
 
     ASSERT(s_query(s2[0], A), "failure in reading second sample");
 }
